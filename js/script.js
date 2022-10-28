2681cb0192c18db9b72b0dd91606d107c5cc8300
Pablo Filipe <81871324+pablofilipe@users.noreply.github.com>
2022-10-28 17:56:25 -0300

    Atualização do script
    Adicionando o código correto.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,88 @@
 let countDownDate = new Date('Nov 8, 2022 18:00:00').getTime()
-let msg = document.getElementById('msg')
+//let countDownDate = new Date('Oct 19, 2022 16:34:00').getTime()
 
-let x = setInterval( () => {
-    let now = new Date().getTime()
-    let distance = countDownDate - now
+function start() {
+    treatDisplay( 'days' )
+    treatDisplay( 'hours' )
+    treatDisplay( 'minutes' )
+    treatDisplay( 'seconds' )
+    time()
+}
 
-    let days = Math.floor( distance / (1000 * 60 * 60 * 24) )
-    let hours = Math.floor( (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) )
-    let minutes = Math.floor( (distance % (1000 * 60 * 60)) / (1000 * 60) )
-    let seconds = Math.floor( (distance % (1000 * 60)) / 1000 )
+function time() {
+    let x = setInterval( () => {
+        let now = new Date().getTime()
+        let distance = countDownDate - now
 
-    if ( distance > 0) {
-        if ( days > 0 ) {
-            document.getElementById('days').innerHTML = `${days}d`
+        let days = Math.floor( distance / (1000 * 60 * 60 * 24) )
+        let hours = Math.floor( (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) )
+        let minutes = Math.floor( (distance % (1000 * 60 * 60)) / (1000 * 60) )
+        let seconds = Math.floor( (distance % (1000 * 60)) / 1000 )
+
+        if ( distance > 0 ) {
+            if ( days > 0 ) {
+                document.getElementById('days').innerHTML = `${days}d`
+            } else {
+                document.getElementById('days').classList.add('hidden')
+            }
+
+            if ( hours > 0 ) {
+                document.getElementById('hours').innerHTML = `${hours}h`
+            } else {
+                document.getElementById('hours').classList.add('hidden')
+            }
+
+            if ( minutes > 0 ) {
+                document.getElementById('minutes').innerHTML = `${minutes}m`
+            } else {
+                document.getElementById('minutes').classList.add('hidden')
+            }
+
+            if ( seconds >= 0 ) {
+                document.getElementById('seconds').innerHTML = `${seconds}s`
+            } else {
+                document.getElementById('seconds').classList.add('hidden')
+            }
         }
 
-        if ( hours > 0 ) {
-            document.getElementById('hours').innerHTML = `${hours}h`
+        if ( ( days === 0 && hours === 0 && minutes === 0 ) && (seconds <= 59) ) {
+            document.getElementById('seconds').classList.add('secondColor')
         }
 
-        if ( minutes > 0 ) {
-            document.getElementById('minutes').innerHTML = `${minutes}m`
+        if ( distance < 0 ) {
+            finish( x )
         }
+    }, 1000)
+}
 
-        if ( seconds >= 0 ) {
-            document.getElementById('seconds').innerHTML = `${seconds}s`
-        }
+function treatDisplay( key ) {
+    let demo = document.getElementById('demo')
+    let elementInfo = document.createElement('span')
+    let msg = document.getElementsByClassName('class_paragraph')
+
+    if ( !msg ) {
+        let paragraph = document.createElement('p')
+        paragraph.id = 'id-paragraph'
+        paragraph.classList.add('paragraph')
+        paragraph.innerText = "Restam"
+        demo.appendChild( paragraph )
     }
 
-    if ( distance < 0 ) {
-        clearInterval( x )
+    elementInfo.id = `${key}`
+    if ( key !== 'days') elementInfo.classList.add('spacing')
+    demo.appendChild( elementInfo )
+}
 
-        if ( msg.parentNode ) {
-            msg.parentNode.removeChild(msg)
-        }
+function finish( x ) {
+    clearInterval( x )
 
-        document.getElementById('demo').innerHTML = `EXPIRED!`
+    let msg = document.getElementsByClassName('class_paragraph')
+
+    if ( msg.parentNode ) {
+        msg.parentNode.removeChild( msg )
     }
-}, 1000)
+
+    document.getElementById('demo').innerHTML = `EXPIRED!`
+}
+
+start()
